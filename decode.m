59f0c50a3b0936842094865275a3b17ed0921ffa
Tu <74278541+tuantu4194@users.noreply.github.com>
2021-06-27 19:39:38 +0700

    Update decode.m

--- decode.m
+++ decode.m
@@ -4,22 +4,15 @@ for i = 1 : len
    for j = 1 : len_unique
        
        % If tag value falls between a certain 
-       % range in lookup table then
        if tag > interval(j, 1) && tag < interval(j, 2)
            pos = j;
            tag = (tag - interval(pos, 1)) / p(j);
-  
-           % Geeting the matched tag 
-           % value character
-           decoded_str = u(pos);
-  
-           % String concatenating 
-           % 'decoded_str' into 'str'
+           decoded_str = u(pos);  
            str = strcat(str, decoded_str);
            break
        end
    end
 end
   
-% Displaying final decoded string
+% display result
 disp(str)
